a79998fe6198f258a39359a97afaf627263a1be5
TammineediTanusri <2420030105@klh.edu.in>
2025-08-06 11:38:57 +0530

    Adding files

---
+++ ReadPrint.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class ReadPrint {
+	 public static void main(String[] args) {
+	        String name = "Tanusri";  
+
+	        System.out.println("Your name: " + name);
+	    }
+
+}
